1936ce52bd337556b4459b920261dceaacdaceeb
김경민 <lifeandmyth@gmail.com>
2023-06-24 16:14:09 +0900

    20230624 model활용 방법 획득

--- know_arc_pjt/keyword_chart/views.py
+++ know_arc_pjt/keyword_chart/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import NewsSites
+from .models import NewsTechworldT
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
 def post_view(request):
-    posts = NewsSites.objects.all() #Post테이블의 모든 객체 불러와서 posts변수에 저장
+    posts = NewsSites.objects.all() #NewsSites테이블의 모든 객체 불러와서 posts변수에 저장
+    #여기서 NewsTechworldT테이블의 데이터도 가져와서 dict 안에 넣는다
     print(posts)
     return render(request, 'index.html', {"posts": posts})
