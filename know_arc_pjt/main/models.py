d5af473eb5fd9aef9563a29412057a27ab2b6f4c
김경민 <lifeandmyth@gmail.com>
2023-06-26 19:28:52 +0900

    20230626 쳌아웃

--- know_arc_pjt/main/models.py
+++ know_arc_pjt/main/models.py
@@ -6,7 +6,21 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+ 
 
+# Create your models here.
+ 
+class CustomUser(AbstractUser):
+
+    # 추가 필드 (career, affiliation)
+     
+    CAREER = (
+        ('주니어', '신입 ~ 5년차'),
+        ('시니어', '5년차 이상')
+    )
+    career = models.CharField(verbose_name='경력', max_length=3, choices=CAREER, unique=True, null=True)
+    affiliation = models.CharField(verbose_name='소속', max_length=256, blank=True)
 
 class AccountEmailaddress(models.Model):
     email = models.CharField(unique=True, max_length=254)
