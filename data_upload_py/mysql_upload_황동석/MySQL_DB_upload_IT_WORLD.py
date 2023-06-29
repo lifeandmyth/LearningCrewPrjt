b296e51c457c74ad272f6ea02562356c61581157
김경민 <lifeandmyth@gmail.com>
2023-06-29 11:22:51 +0900

    20230629 다시 재업

---
+++ data_upload_py/mysql_upload_황동석/MySQL_DB_upload_IT_WORLD.py
@@ -0,0 +1,93 @@
+#==============================
+# mySQL db에 업로드하기
+
+import pymysql
+from mydb_local_env import *
+conn = pymysql.connect(host=host, port=port, user=user, password=password, charset=charset)
+
+# with conn:
+#   cur = conn.cursor()
+#   # DB 만들기
+#   sql_db_drop = """
+#     DROP DATABASE IF EXISTS news_cwl_data;
+#   """
+#   sql_db = """
+#     CREATE DATABASE news_cwl_data DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+#   """
+#   cur.execute(sql_db_drop)
+#   cur.execute(sql_db)
+#   conn.commit()
+#   # 해당 연결의 현존하는 db 목록 뽑기
+#   cur.execute("SHOW DATABASES")
+#   # for data in cur:
+  #   print(data)
+
+db = "news_cwl_data"
+conn = pymysql.connect(host=host, port=port, user=user, password=password, db=db, charset=charset)
+# 커서 만들기
+cur = conn.cursor()
+
+
+# sql_cwn_data_t 테이블 만들기
+sql_itworld_data_t ="""
+CREATE TABLE IF NOT EXISTS sql_itworld_data(
+idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+news_id int NOT NULL, 
+url_in VARCHAR(100) NOT NULL,
+thumb_addr VARCHAR(250) NOT NULL,
+news_title VARCHAR(80) NOT NULL, 
+news_text_sm MEDIUMTEXT NOT NULL, 
+news_key VARCHAR(30) NOT NULL,
+news_date VARCHAR(20) NOT NULL, 
+news_writer CHAR(20) NOT NULL,
+tags_string VARCHAR(150)
+)
+"""
+
+# # sql_cwn_data_t 실행하기
+# cur.execute(sql_cwn_data_t)
+# sql_itbz_data_t 실행하기
+cur.execute(sql_itworld_data_t)
+
+# csv파일을 넣기
+import csv
+
+# # cwn csv 읽어오기
+# f = open('know_arc_pjt/data_crawling_D/data_crawling_김경민/cwn.kr/2023-06-20-12.csv','r', encoding='utf8')
+# itbiz csv tech 
+f = open('know_arc_pjt/data_crawling_D/data_crawling_황동석/IT_WORLD_Crawling_최종_처리된.CSV','r', encoding='utf8')
+rdr = csv.reader(f)
+next(rdr)
+
+# 2차원 리스트 만들기
+total_news_list = []
+for line in rdr:
+    total_news_list.append(line)
+    # print(total_news_list)
+f.close()
+
+
+# total_news_list = []
+# for a in news_t_list:
+#     # a.append(now_t)
+#     total_news_list.append(a)
+# # print(total_news_list) 
+
+# # cwn_cwl_data에 데이터 입력하기
+# cur.executemany("INSERT INTO cwn_cwl_data(news_date, news_title, news_text_sm, url_in, news_writer, tags_string, thumb_addr) VALUES (%s,%s,%s,%s,%s,%s,%s)", total_news_list)
+
+# itbz_cwl_data에 데이터 입력하기
+cur.executemany("INSERT INTO sql_itworld_data(news_id, url_in, thumb_addr, news_title, news_text_sm, news_key, news_date, news_writer ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", total_news_list)
+
+# news_id int NOT NULL, 
+# url_in VARCHAR(100) NOT NULL,
+# thumb_addr VARCHAR(100) NOT NULL,
+# news_title VARCHAR(80) NOT NULL, 
+# news_text_sm MEDIUMTEXT NOT NULL, 
+# news_key VARCHAR(30) NOT NULL,
+# news_date VARCHAR(20) NOT NULL, 
+# news_writer CHAR(20) NOT NULL,
+# tags_string VARCHAR(150)
+
+# db 적용, 트랜젝션 종료
+conn.commit()
---
+++ 최종보고서/ERD/키워드 추출 및 단어별 자연어 처리 DB.sql
@@ -0,0 +1,69 @@
+﻿CREATE TABLE `news_t_table` (
+	`seq`	int	NOT NULL	COMMENT 'AUTO INCREMENT',
+	`Key`	VARCHAR(30)	NOT NULL,
+	`title_keywords`	VARCHAR(30)	NOT NULL,
+	`text`	VARCHAR(100)	NOT NULL,
+	`addr`	VARCHAR(50)	NOT NULL,
+	`thumbnail_addr`	VARCHAR(100)	NOT NULL,
+	`date`	datetime	NOT NULL	COMMENT '중복 가능',
+	`tag`	VARCHAR(40)	NULL	COMMENT '중복 가능',
+	`author`	CHAR(4)	NOT NULL	COMMENT '중복 가능'
+);
+
+CREATE TABLE `title_n_key` (
+	`seq`	int	NOT NULL,
+	`title`	VARCHAR(30)	NOT NULL,
+	`title_keywords`	VARCHAR(30)	NOT NULL	COMMENT '전체 목록을 하나의 string으로',
+	`text`	VARCHAR(100)	NOT NULL,
+	`addr`	VARCHAR(50)	NOT NULL,
+	`thumbnail_addr`	VARCHAR(100)	NOT NULL	COMMENT '썸네일 이미지 주소',
+	`date`	datetime	NOT NULL	COMMENT '작성일자'
+);
+
+CREATE TABLE `title_n_tag` (
+	`seq`	int	NOT NULL,
+	`title`	VARCHAR(30)	NOT NULL,
+	`tag`	VARCHAR(40)	NULL	COMMENT '중복 가능'
+);
+
+CREATE TABLE `title_n_author` (
+	`seq`	int	NOT NULL,
+	`title`	VARCHAR(30)	NOT NULL,
+	`author`	CHAR(4)	NOT NULL	COMMENT '중복 가능',
+	`date`	DATETIME	NOT NULL	COMMENT '중복 가능'
+);
+
+ALTER TABLE `news_t_table` ADD CONSTRAINT `PK_NEWS_T_TABLE` PRIMARY KEY (
+	`seq`,
+	`Key`
+);
+
+ALTER TABLE `title_n_key` ADD CONSTRAINT `PK_TITLE_N_KEY` PRIMARY KEY (
+	`seq`,
+	`title`
+);
+
+ALTER TABLE `title_n_tag` ADD CONSTRAINT `PK_TITLE_N_TAG` PRIMARY KEY (
+	`seq`,
+	`title`
+);
+
+ALTER TABLE `title_n_author` ADD CONSTRAINT `PK_TITLE_N_AUTHOR` PRIMARY KEY (
+	`seq`,
+	`title`
+);
+
+ALTER TABLE `title_n_tag` ADD CONSTRAINT `FK_title_n_key_TO_title_n_tag_1` FOREIGN KEY (
+	`title`
+)
+REFERENCES `title_n_key` (
+	`title`
+);
+
+ALTER TABLE `title_n_author` ADD CONSTRAINT `FK_title_n_key_TO_title_n_author_1` FOREIGN KEY (
+	`title`
+)
+REFERENCES `title_n_key` (
+	`title`
+);
+
